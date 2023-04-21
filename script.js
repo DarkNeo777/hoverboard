7cb49c4b1bff9a9ec73aafb89854b4a789334178
Godspeed <ecm.corp78@gmail.com>
2023-04-20 23:15:21 -0400

    when click create cross of color

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const container = document.getElementById('container');
 
-const colors = ['#e11d48','#c026d3','#4f46e5','#0d9488','#ca8a04','#dc2626'];
+const colors = ['#e11d48', '#c026d3', '#4f46e5', '#0d9488', '#ca8a04', '#dc2626'];
 
 const SQUARES = 500;
 
-for(let i = 0; i < SQUARES; i++) {
+for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    
+
+    square.id = i
 
     square.addEventListener('mouseover', () => setColor(square));
     square.addEventListener('mouseout', () => removeColor(square));
+    square.addEventListener('click', () => line(square));
 
     container.appendChild(square);
 }
@@ -24,4 +26,17 @@ function setColor(square) {
 function removeColor(square) {
     square.style.backgroundColor = '#1d1d1d';
     square.style.boxShadow = '0 0 2px #000'
+}
+
+function line(square) {
+    const id = square.id;
+    for (let i = 0; i < SQUARES; i++) {
+        const block = document.getElementById(i);
+        if (i == id || i % 20 == id % 20 || Math.floor(i/20) == Math.floor(id/20)) {
+            setColor(block);
+        } else {
+            removeColor(block);
+        }
+    }
+
 }
